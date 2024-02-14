93bf293f1e6f327a382a15fe0ab7ef385a4ce94b
dudtjs126 <dudtjs126@gmail.com>
2024-02-14 16:59:50 +0900

    [Feat] 아이템 랜덤 위치 생성 구현

--- Assets/Scripts/ItemBox/ItemManager.cs
+++ Assets/Scripts/ItemBox/ItemManager.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class ItemManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject itemBox;
+
+    public void ItemGridPosition()
     {
-        
+        for (int i = 0;i < 2; i++)
+        {
+            InvokeRepeating("itemBoxCreate", 0.0f, 0.0f);
+
+            for (int j = 0; j < 2; j++)
+            {
+                float itemPosX = Random.Range(-8.0f, 8.0f);
+                float itmePosY = Random.Range(-4.5f, 3.5f);
+                transform.position = new Vector3(itemPosX, itmePosY, 0);
+                Debug.Log("Ãâ·ÂµÊ");
+            }
+
+            if (i >= 3)
+            {
+                break;
+            }
+        }
     }
 
-    // Update is called once per frame
+    private void itemBoxCreate()
+    {
+        Instantiate(itemBox);
+    }
+
+
+    void Start()
+    {
+        ItemGridPosition();
+    }
+
+
     void Update()
     {
-        
+
     }
 }
