4d4316813dab740d9205e27464bb8ff390db3ffd
dudtjs126 <dudtjs126@gmail.com>
2024-02-15 11:50:03 +0900

    [Feat] 보물상자 사라지고 아이템 생성되는 것 구현 (위치는 아직 미구현)

--- Assets/Scripts/ItemBox/ItemManager.cs
+++ Assets/Scripts/ItemBox/ItemManager.cs
@@ -6,17 +6,20 @@ using UnityEngine.Tilemaps;
 public class ItemManager : MonoBehaviour
 {
     public GameObject itemBox;
-
+    public int randomNumber;
+    public GameObject powerUp;
+    public GameObject powerSpeedUp;
+    public GameObject speedUp;
+    public GameObject hpCure;
 
     public void ItemGridPosition()
     {
         for (int i = 0; i < 2; i++)
         {
-            Invoke("itemBoxCreate", 0.0f);
-            float itemPosX = Random.Range(-8.0f, 8.0f);
-            float itmePosY = Random.Range(-4.5f, 3.5f);
+            Invoke("ItemBoxCreate", 0.0f);
+            float itemPosX = Random.Range(-7.0f, 7.0f);
+            float itmePosY = Random.Range(-2.5f, 2.5f);
             transform.position = new Vector3(itemPosX, itmePosY, 0);
-            Debug.Log("Ãâ·ÂµÊ");
 
             if (i >= 3)
             {
@@ -25,10 +28,45 @@ public class ItemManager : MonoBehaviour
         }
     }
 
-    private void itemBoxCreate()
+    private void ItemBoxCreate()
     {
         Instantiate(itemBox);
     }
+
+    private void OnCollisionEnter2D(Collision2D itemBox)
+    {
+            if (itemBox.gameObject.tag == "Player")
+            {
+                /*Instantiate(ItemRandomSpawn);*/
+                ItemRandomSpawn();
+                Debug.Log(itemBox.transform.position);
+            }
+    }
+
+
+    private void ItemRandomSpawn()
+    {
+        randomNumber = Random.Range(0, 3);
+
+        if (randomNumber == 0)
+        {
+            Instantiate(powerUp);
+        }
+        else if (randomNumber == 1)
+        {
+            Instantiate(powerSpeedUp);
+        }
+        else if (randomNumber == 2)
+        {
+            Instantiate(speedUp);
+        }
+        else
+        {
+            Instantiate(hpCure);
+        }
+    }
+
+
 
 
     void Start()
